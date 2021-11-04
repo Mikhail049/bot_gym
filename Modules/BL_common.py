4003495a55bb1cf3eb3f202fff81be0967137dae
Mikhail049 <mikhail.worker49@gmail.com>
2021-11-05 03:34:51 +0600

    mysql correct

--- Modules/BL_common.py
+++ Modules/BL_common.py
@@ -1,10 +1,21 @@
 import Modules.MySqlHelper as msql
 
-def parsingMessage(message):
-    validateExerciseName(message)
+from aiogram import types
+
+async def parsingMessage(message: types.Message):
+    text = message.text
+    userId = message.from_user
+    validateExerciseName(text)
     return 0
 
 
-def validateExerciseName(exerciseName):
+async def validateExerciseName(exerciseName):
     msql.getNameExcercise(exerciseName)
+    return 0
+
+async def addExerciseToSteck():
+
+    return 0
+
+async def validateUserAccess(UserID):
     return 0
--- Modules/MySqlHelper.py
+++ Modules/MySqlHelper.py
@@ -4,11 +4,15 @@ import os
 from datetime import datetime
 
 
+connectionParameters = {
+    'user': os.getenv("USER_NAME_MYSQL"),
+    'password': os.getenv("PASS_MYSQL"),
+    'host': os.getenv("MYSQL_HOST"),
+    'database': os.getenv("DB_NAME_MYGYM")
+}
+
 def connectionToDb():
-    return mysql.connector.connect(user = os.getenv("USER_NAME_MYSQL"),
-                              password= os.getenv("PASS_MYSQL"),
-                              host= os.getenv("MYSQL_HOST"),
-                              database=os.getenv("DB_NAME_MYGYM"))
+    return mysql.connector.connect(connectionParameters)
 
 
 def getNameExcercise(exerciseName):
@@ -22,15 +26,30 @@ def getNameExcercise(exerciseName):
     pprint.pprint(result)
     return result
 
+def getUser(userID) -> str:
+    commandString = 'SELECT user_name FROM dt_main_data_about_users WHERE id= %s'
+    data : tuple = (str(userID),)
+    cnx = connectionToDb()
+    cursor = cnx.cursor(prepared=True)
+    cursor.execute(commandString, data)
+    result = cursor.fetchone()
+    cnx.close()
+    if not (result is None) and len(result) > 0:
+        return result[0]
+    else:
+        return ''
 
-def addExerciseToStack(user, exercise, reps, minutes, weight):
-    commandS = "INSERT INTO dt_mapping_exercises (user, exercise, date, reps, minutes, weight) " \
-               "VALUE(%s, %s, %s, %s);"
+def addExerciseToStack(user, exercise, reps = None, minutes = None, weight = None):
+    commandS = "INSERT INTO dt_exercises_steck (user, exercise, date, reps, minutes, weight) " \
+               "VALUE(%s, %s, %s, %s, %s, %s);"
     data = (user, exercise, datetime.now(), reps, minutes, weight)
     cnx = connectionToDb()
-    cursor = cnx.cursor()
+    cursor = cnx.cursor(prepared=True)
     cursor.execute(commandS, data)
+    cnx.commit()
     cnx.close()
+
+
 
 
 def addUserToDB():
